c0b6092e0f95bfba0814f316a53fa91088a43903
hoang hai lam <hoanghailam2010q@gmail.com>
2026-06-22 09:46:41 +0700

    Create mailing.js

---
+++ server/constants/mailing.js
@@ -0,0 +1,7 @@
+module.exports = {
+  smtpHost: "in-v3.mailjet.com",
+  smtpPort: 587,
+  secure: false,
+  username: "8ae0420db75449376487d9c787671f12",
+  password: "eecb6f7e9c9684edb71f6a2fc1eba30b"
+};
